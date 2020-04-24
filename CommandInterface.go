dbca334d7f1a15d073ba0eeb3ac9929ae1e0315d
Jagepard <Jagepard@yandex.ru>
2020-04-24 22:21:04 +0300

    Update all

--- CommandInterface.go
+++ CommandInterface.go
@@ -1,10 +1,11 @@
+/**
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
+ */
+
 package main
 
 // CommandInterface is ...
 type CommandInterface interface {
 	exec()
-}
-
-func exec(command CommandInterface) {
-	command.exec()
 }
--- DeviceInterface.go
+++ DeviceInterface.go
@@ -1,15 +1,12 @@
+/**
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
+ */
+
 package main
 
 // DeviceInterface is ...
 type DeviceInterface interface {
 	setCommand(commandName string, command CommandInterface)
 	execute(commandName string)
-}
-
-func execute(device DeviceInterface, commandName string) {
-	device.execute(commandName)
-}
-
-func setCommand(device DeviceInterface, commandName string, command CommandInterface) {
-	device.setCommand(commandName, command)
 }
