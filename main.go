db22c4b5e50185bd84428a0c831fb34e3d21f40b
Jagepard <Jagepard@yandex.ru>
2020-04-24 22:04:46 +0300

    Add device

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
+/**
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
+ */
+
 package main
 
 func main() {
 	device := Lamp{commands: make(map[string]CommandInterface)}
 
-	setCommand(device, "on", TurnOnCommand{})
-	setCommand(device, "off", TurnOffCommand{})
-	setCommand(device, "toggle", ToggleCommand{})
+	device.setCommand("on", TurnOnCommand{})
+	device.setCommand("off", TurnOffCommand{})
+	device.setCommand("toggle", ToggleCommand{})
 
-	execute(device, "on")
-	execute(device, "off")
-
-	execute(device, "toggle")
-	execute(device, "toggle")
+	device.execute("on")
+	device.execute("off")
+	device.execute("toggle")
+	device.execute("toggle")
 }
